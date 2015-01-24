5573dd593aa06e0eba9f220ec1ea5e3e4843d791
Hao Wang <hw1@cs.cmu.edu>
2015-01-23 19:59:26 -0500

    full analysis pipeline generated

--- analysis/run_analysis.sh
+++ analysis/run_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# cd ~/scratch/scratch2/ribomapanalysis/
-# ./hela_ribo_analysis.sh
+cd ~/scratch/scratch2/ribomapanalysis/
+./hela_ribo_analysis.sh
 cd ~/scratch/scratch2/software_testing/ribomap/scripts/footprint_generator/
 ./footprint_generation_pipe.sh
 cd ~/scratch/scratch2/ribomapanalysis/
